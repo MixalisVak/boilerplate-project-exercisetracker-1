f279d7aa25304698d33d06646dbf95c16c4106ee
Mixalis Vakalos <vakalosm@qc-analytics.com>
2024-08-25 18:54:26 +0300

    first-commit API

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const { v4: uuidv4 } = require('uuid');
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+
+app.post('/api/users', (req, res) => {
+    console.log('req.quer', req.body)
+    let userName = req.body.username
+    console.log(userName, uuidv4())
+    res.json({
+        username: userName,
+        "_id": uuidv4()
+    })
 });
 
 
